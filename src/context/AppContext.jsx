50c074c057626cad56e544b53ca7791a634ac524
mrgowtham120703 <gowthammr120703@gmail.com>
2025-10-29 17:24:45 -0700

    AppContext.jsx light/dark mode created

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { dummyUserData } from "../assets/assets";
+import { dummyChats, dummyUserData } from "../assets/assets";
 
 const AppContext = createContext()
 
@@ -17,11 +17,36 @@ export const AppContextProvider = ({ children }) => {
         setUser(dummyUserData)
     }
 
+    const fetchUserChats = async () => {
+        setChats(dummyChats)
+        setSelectedChats(dummyChats[0])
+    }
+
+    useEffect(()=>{
+        if(theme === 'dark'){
+            document.documentElement.classList.add('dark');
+        }else{
+            document.documentElement.classList.remove('dark')
+        }
+    },[theme])
+
+    useEffect(()=>{
+      if(user){
+        fetchUserChats()
+      } else {
+        setChats([])
+        setSelectedChats(null)
+      } 
+    },[user])
+
     useEffect(()=>{
         fetchUser()
     },[])
 
-    const value = {}
+    const value = {
+        navigate, user, setUser, fetchUser, chats, setChats, selectedChat, 
+        selectedChat, theme
+    }
 
     return (
         <AppContext.Provider>
